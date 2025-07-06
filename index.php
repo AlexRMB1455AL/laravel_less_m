ab50430b07f95b43781c3530034c0a5b12d003e0
ALEKSANDR.I <alenamobilebkru@gmail.com>
2025-07-06 17:22:12 +0400

    https://github.com/AlexRMB1455AL/laravel_less_m.git

--- index.php
+++ index.php
@@ -9,7 +9,17 @@ $petName = 'Barsik';
 
 echo $name . "\n", $age . "\n", $hobby . "\n", $isMarried . "\n", $petName;
 
+$nameVictor = 'Victor';
+$ageVictor = 34;
+$hobbyVictor = 'running';
+$isMarriedVictor = false;
+$petNameVictor = 'Sharik';
+echo "\n" . $nameVictor . "\n", $ageVictor . "\n", $hobbyVictor . "\n", $isMarriedVictor . "\n", $petNameVictor;
 
-
-
+$nameElena = 'Elena';
+$ageElena = 16;
+$hobbyElena = 'reading';
+$isMarriedElena = false;
+$petNameElena = 'Murka';
+echo "\n" . $nameElena . "\n", $ageElena . "\n", $hobbyElena . "\n", $isMarriedElena . "\n", $petNameElena;
 
