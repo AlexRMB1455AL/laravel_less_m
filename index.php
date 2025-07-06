80624c4416487410ded9f7b701bf9d924cc13bb3
ALEKSANDR.I <alenamobilebkru@gmail.com>
2025-07-06 17:16:50 +0400

    https://github.com/AlexRMB1455AL/laravel_less_m.git

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+//echo 'fjfhgbjnfgjhkjjk57879khkh';
+
+$name = 'Victor';
+$age = 18;
+$hobby = 'swimming';
+$isMarried = true;
+$petName = 'Barsik';
+
+echo $name . "\n", $age . "\n", $hobby . "\n", $isMarried . "\n", $petName;
+
+
+
+
+
